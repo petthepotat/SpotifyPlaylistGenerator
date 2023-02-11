499c0aad4c86ce1eeb74a3413f7a496bed59cf9e
Peter Zhang <ultraswordkirbypink@gmail.com>
2023-02-11 10:09:39 -0500

    removed some audio files

--- scripts/ffmpeg.py
+++ scripts/ffmpeg.py
@@ -1,8 +1,16 @@
 import subprocess
 import sys
 
+from typing import List
+import PIL
+
 
 """
+
+
+ffmpeg -i input1.mp3 -t 15 -i input2.mp3 -t 15 -filter_complex "[0:0][1:0]concat=n=2:v=0:a=1[out]" -map "[out]" output.mp3
+
+
 ffmpeg options:
     -i: input file
     -c:v: video codec
@@ -56,15 +64,45 @@ CONCATFILE = '-f concat -i ".blob/concat.txt"'
 # functions
 
 
-class FFmpeg:
-    def __init__(self, target: str, **kwargs):
-        """FFmpeg handler"""
-        self.options = {**kwargs}
-        # valid kwargs:
+class FFmpegImageVideoGenerator:
+    @classmethod
+    def make_pair(cls, command: str, value: str):
+        """Make a pair of a command and a value"""
+        return f"{command} {value}"
+
+    # ------------------------------- #
+
+    def __init__(self, image_file: str, input_files: List[str]):
         """
-        above lmao go check or smth idk
+        FFmpeg handler
+        given:
+        - input image (the background of the entire video)
+        - file (contains all the audio tracks to be joined together)
+        - duration (the duration of the video)
+        - output file (the output file)
         """
-        self.target = target
+        self.target_file = target
+        # args
+        self.input_file = self.make_pair(TARGET_IMG, value)
+        self.video_codec = ""
+
+    def run_process(self):
+        """Run the ffmpeg process"""
+        # build the command
+        command = [FFMPEGPATH]
+        # add the options
+        for key, value in self.options.items():
+            command.append(key)
+            command.append(value)
+        # add the target
+        command.append(self.target)
+
+        # run the process
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        # read output
+        output, error = process.communicate()
+        print(output.decode().strip())
+        print(error)
 
 
 # ------------------------------- #
